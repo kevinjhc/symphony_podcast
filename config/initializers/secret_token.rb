72c9bf76b66773985c06aebfd35b0bcc3ba50de3
Kevin Chang <kevinjhc@gmail.com>
2015-09-02 14:09:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SymphonyPodcast::Application.config.secret_key_base = 'a51583e3d81cf4cc3b551b57f63e35e65c31ec4f7b9891b4d1dd6694246268312d023117ae875bfaa816ffe4bcfee1654d9bfa535594f0d360a0035f3d3ad19d'
